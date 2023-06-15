a97ca80bed0928afa63ca6be4e1832e877fa0e57
AhmedHossam <ahmedhossam6417@gmail.com>
2023-06-16 02:33:40 +0300

    new

---
+++ problem2.js
@@ -0,0 +1,14 @@
+const filesystem = require('fs');
+const changeDate=(data)=>{
+    let accidentsList=Array.from(data.accidents);
+    console.log(accidentsList)
+    for(let i=0 ; i<accidentsList.length;i++){
+        accidentsList[i].date=moment(accidentsList[i].date, "MM/DD/YYYY").format("YYYY-MM-DD");
+    }
+}
+const moment = require('moment');
+const data = JSON.parse(filesystem.readFileSync('./problem2.json',{'encoding':'utf-8'}));
+changeDate(data);
+console.log(data);
+filesystem.writeFileSync('output2.json',JSON.stringify(data),{'encoding':'utf-8'});
+
